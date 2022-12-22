f58d013ed69e4d0568605485ee3d7069a9b5d336
erichill41 <ehill4182@yahoo.com>
2022-12-21 17:51:40 -0700

    complete chapter 2

--- code/02_python.py
+++ code/02_python.py
@@ -5,6 +5,8 @@
 ##########################
 # how to read this chapter
 ##########################
+from os import path
+import os
 1 + 1
 
 ##########
@@ -12,7 +14,7 @@
 ##########
 
 # print the result of 1 + 1
-print(1 + 1)
+# print(1 + 1)
 
 ###########
 # variables
@@ -87,11 +89,11 @@ meh = not (shootout or at_least_one_good_team or you_guys_are_bad)
 # if statements
 ###############
 if team1_won:
-  message = "Nice job team 1!"
+    message = "Nice job team 1!"
 elif team2_won:
-  message = "Way to go team 2!!"
+    message = "Way to go team 2!!"
 else:
-  message = "must have tied!"
+    message = "must have tied!"
 
 message
 
@@ -102,9 +104,9 @@ message
 # lists
 first_line = ['alex ovechkin', 'nicklas backstrom', 'anthony mantha']
 
-first_line[0]
-first_line[0:2]
-first_line[-2:]
+first_line[0] #prints alex ovechkin
+first_line[0:2] #prints indexes 0 and 1
+first_line[-2:] #prints last 2 items in list
 
 # dicts
 first_line_dict = {'lw': 'alex ovechkin',
@@ -113,8 +115,15 @@ first_line_dict = {'lw': 'alex ovechkin',
 
 first_line_dict['lw']
 first_line_dict['rd'] = 'john carlson'
+first_line_dict['ld'] = 'dimitry orlov'
+first_line_dict['g'] = 'darcy kuemper'
 
-first_line_dict
+first_line_positions = [
+    player for _, player in first_line_dict.items()
+        if player.split(' ')[-1][0] in ['b', 'm']
+]
+print('alex ovechkin'.split(' ')[-1][0])
+print(first_line_dict)
 
 pos = 'lw'
 first_line_dict[pos]
@@ -147,8 +156,8 @@ for x in first_line_dict:
     print(f"position: {x}")
 
 for x in first_line_dict:
-   print(f"position: {x}")
-   print(f"player: {first_line_dict[x]}")
+    print(f"position: {x}")
+    print(f"player: {first_line_dict[x]}")
 
 for x, y in first_line_dict.items():
     print(f"position: {x}")
@@ -189,7 +198,7 @@ first_line_a_only_title
 
 # dicts
 salary_per_player = {
-    'alex ovechkin': 10000000 , 'nicklas backstrom': 12000000, 'anthony mantha': 5700000}
+    'alex ovechkin': 10000000, 'nicklas backstrom': 12000000, 'anthony mantha': 5700000}
 
 salary_m_per_upper_player = {
     name.upper(): salary/1000000 for name, salary in salary_per_player.items()}
@@ -210,6 +219,7 @@ n_goals
 
 4 + len(['alex ovechkin', 'nicklas backstrom', 'anthony mantha'])
 
+
 def team_pts(wins, losses, ot_losses):
     """
     multi line strings in python are between three double quotes
@@ -221,10 +231,12 @@ def team_pts(wins, losses, ot_losses):
     """
     return 2*wins + 1*ot_losses
 
+
 team_pts(62, 16, 4)
 
 # this gives an error: wins is only defined inside team_pts
 # print(wins)
+
 
 def team_pts_noisy(wins, losses, ot_losses):
     """
@@ -236,9 +248,12 @@ def team_pts_noisy(wins, losses, ot_losses):
     print(wins)  # works here since we're inside fn
     return 2*wins + 1*ot_losses
 
+
 team_pts_noisy(62, 16, 4)
 
 # side effects
+
+
 def is_player_on_team(player, team):
     """
     take a player string and team list and check whether the player is on team
@@ -249,6 +264,7 @@ def is_player_on_team(player, team):
     team.append(player)
     return team.count(player) >= 2
 
+
 roster_list = ['alex ovechkin', 'nicklas backstrom', 'anthony mantha']
 is_player_on_team('sidney crosby', roster_list)
 
@@ -258,7 +274,7 @@ is_player_on_team('sidney crosby', roster_list)
 roster_list
 
 # function arguments
-## Positional vs Keyword Arguments
+# Positional vs Keyword Arguments
 
 team_pts(62, 16, 4)
 team_pts(62, 4, 16)  # order matters!
@@ -272,10 +288,11 @@ team_pts(62, losses=16, ot_losses=4)
 # error: keyword arguments can't come before positional arguments
 # team_pts(ot_losses=4, 62, 16)
 
-## Default Values for Arguments
+# Default Values for Arguments
 
 # error: leaving off an argument
 # team_pts(62, 16)
+
 
 def team_pts_wdefault(wins, losses, ot_losses=0):
     """
@@ -283,6 +300,7 @@ def team_pts_wdefault(wins, losses, ot_losses=0):
     losses and returns team points
     """
     return 2*wins + 1*ot_losses
+
 
 team_pts_wdefault(62, 16)
 
@@ -315,11 +333,13 @@ def do_to_list(working_list, working_fn, desc):
 
     return f'{desc} {value}'
 
+
 def last_elem_in_list(working_list):
     """
     returns the last element of a list.
     """
     return working_list[-1]
+
 
 positions = ['LW', 'C', 'RW', 'LD', 'RD', 'GK']
 
@@ -328,16 +348,70 @@ do_to_list([1, 2, 4, 8], last_elem_in_list, "last element in your list:")
 
 do_to_list(positions, len, "length of your list:")
 
-do_to_list([2, 3, 7, 1.3, 5], lambda x: 3*x[0], "first element in your list times 3 is:")
+do_to_list([2, 3, 7, 1.3, 5], lambda x: 3*x[0],
+           "first element in your list times 3 is:")
 
 # normally imports like this would be at the top of the file
-import os
-
+from os import path
 os.cpu_count()
 
-from os import path
 
 # change this to the location of your data
-DATA_DIR = '/Users/nathan/fantasybook/data'
+DATA_DIR = '/Users/erichill/code-hockey-files/data'
 path.join(DATA_DIR, 'adp_2017.csv')
 os.path.join(DATA_DIR, 'adp_2017.csv')  # alt if we didn't want to import path
+
+
+def announcement(player, stat):
+    return f'{player} with the {stat}'
+
+announcement('Crosby', 'penalty')
+
+def is_oreilly(player):
+    return player.replace("'", '').lower() == 'ryan oreilly'
+
+def a_lot_of_goals(goals):
+    if goals >= 4:
+        return f'{goals} is a lot of goals!'
+    else:
+        return f'{goals} is not many, try harder'
+
+a_lot_of_goals(6)
+
+roster_list_alt = ['alex ovechkin', 'nicklas backstrom', 'anthony mantha']
+## using comprehension to turn list into dict
+roster_list_alt_dict = {
+    player.title(): len(player) for player in roster_list_alt
+}
+#print(roster_list_alt_dict)
+#print(roster_list_alt[:2])
+#print([player.title() for player in roster_list_alt][:2])
+#print([player.title() for _, player in first_line_dict.items()][:2])
+
+for player in roster_list_alt:
+    print(player.split(' ')[1])
+
+shot_info = {
+    'shooter': 'Connor McDavid',
+    'is_slap': False,
+    'went_in': False
+}
+#shot_info['shooter'] = 'Nathan Mackinnon'
+
+# showing how to flip a value of a dict
+#def toggle_slap(shot):
+#   shot['is_slap'] = not shot['is_slap']
+#   return shot
+#
+#print(toggle_slap(shot_info))
+shot_info['is_penalty_shot'] = True
+shot_info['distance'] = 20
+print(shot_info)
+
+
+shots = [48, 40, 38, 39, 40, 52]
+
+def mapper(my_list, my_function):
+    return [my_function(x) for x in my_list]
+
+mapper(shots, lambda x: round(x*0.09))
